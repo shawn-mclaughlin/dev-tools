9061c13badebd05689275811a43282c26d2e5394
Shawn McLaughlin <shawn@shawnmclaughlin.dev>
2023-06-23 08:58:35 -0400

    Add postman to tools

--- tools/install-tools.sh
+++ tools/install-tools.sh
@@ -13,6 +13,9 @@ brew install --cask iterm2
 # Install Visual Studio Code
 brew install --cask visual-studio-code
 
+# Install Postman
+brew install --cask postman
+
 # Install OhMyZsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
@@ -21,5 +24,4 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 echo '# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.' >> ~/.zshrc
 echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >> ~/.zshrc
-
 cp $SCRIPT_DIR/.p10k.zsh ~
