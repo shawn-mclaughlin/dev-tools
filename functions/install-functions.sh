3582aa094595f825cb39d91ca19e3df1f24fc4df
Shawn McLaughlin <smclaughlin@scoir.com>
2024-09-19 10:50:59 -0400

    remove functions as ohmyzsh has aliases to cover them

--- functions/install-functions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-SCRIPTDIR=$(pwd)
-
-cp -R ./functions/my-functions ~/.my-functions
-
-echo "\n# Register my zsh functions" >> ~/.zshrc
-echo 'fpath=( ~/.my-functions/**/*(F) "${fpath[@]}" )'  >> ~/.zshrc
-echo 'autoload -Uz ~/.my-functions/**/*(.:t)'  >> ~/.zshrc
